b8c05aca711929e67fce780c25f6adc1589b2bee
Vicente <pouyio@gmail.com>
2024-01-21 22:59:17 +0100

    added live grep, needs install ripgrep

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     local builtin = require("telescope.builtin")
     vim.keymap.set("n", "<leader>p", builtin.find_files, { desc = "Find Files" })
-    -- vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Grep String" })
+    vim.keymap.set('n', '<leader>f', builtin.live_grep, { desc = "Grep String" })
     -- vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Grep word" })
     -- vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "Find old files" })
   end
