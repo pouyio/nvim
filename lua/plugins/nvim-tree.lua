1784ca8576960c791c094493188cb1d18618cedc
Vicente <pouyio@gmail.com>
2024-01-21 16:53:15 +0100

    added tree

---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,35 @@
+return {
+  "nvim-tree/nvim-tree.lua",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    local nvimtree = require("nvim-tree")
+
+    -- recommended settings from nvim-tree documentation
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+
+    -- configure nvim-tree
+    nvimtree.setup({
+      view = {
+        width = 35,
+        side = "right",
+      },
+      -- disable window_picker for
+      -- explorer to work well with
+      -- window splits
+      actions = {
+        open_file = {
+          window_picker = {
+            enable = false,
+          },
+        },
+      },
+      filters = {
+        custom = { ".DS_Store" },
+      },
+    })
+
+
+    vim.keymap.set("n", "<C-b>", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
+  end,
+}
