c435d0100a1f585d463b603205328ed8a808c60b
Vicente <pouyio@gmail.com>
2024-01-23 21:54:24 +0100

    make telescope fullscreen

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,6 +3,13 @@ return {
       dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
     local builtin = require("telescope.builtin")
+    local telescope = require("telescope")
+
+    telescope.setup {
+      defaults = {
+        layout_config = {width = 0.999, height = 0.999}
+      },
+    }
     vim.keymap.set("n", "<leader>p", builtin.find_files, { desc = "Find Files" })
     vim.keymap.set('n', '<leader>f', builtin.live_grep, { desc = "Grep String" })
     -- vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Grep word" })
