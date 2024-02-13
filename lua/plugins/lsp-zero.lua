cb1c0dce67bd0f028754dd6dc71d55d981249dbd
Vicente <pouyio@gmail.com>
2024-02-14 00:55:51 +0100

    added basic lsp-zero config

---
+++ lua/plugins/lsp-zero.lua
@@ -0,0 +1,55 @@
+return {
+  "VonHeikemen/lsp-zero.nvim",
+  dependencies = {
+    'williamboman/mason.nvim',
+    'williamboman/mason-lspconfig.nvim',
+    'neovim/nvim-lspconfig',
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/nvim-cmp',
+    'L3MON4D3/LuaSnip',
+  },
+  init = function()
+    local lsp_zero = require("lsp-zero")
+
+    lsp_zero.on_attach(function(client, bufnr)
+      lsp_zero.default_keymaps({buffer = bufnr})
+    end)
+
+    local cmp = require('cmp')
+    local cmp_action = lsp_zero.cmp_action()
+
+    cmp.setup({
+      mapping = cmp.mapping.preset.insert({
+        -- `Enter` key to confirm completion
+        ['<CR>'] = cmp.mapping.confirm({select = false}),
+
+        -- Ctrl+Space to trigger completion menu
+        -- if set to <C-i> breaks tab in insert mode
+        ['<C-Space>'] = cmp.mapping.complete(),
+        -- Navigate between snippet placeholder
+        ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+        ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+
+        -- Scroll up and down in the completion documentation
+        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-d>'] = cmp.mapping.scroll_docs(4),
+      }),
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      }
+    })
+
+    require('mason').setup({})
+    require('mason-lspconfig').setup({
+      handlers = {
+        lsp_zero.default_setup,
+        lua_ls = function()
+          -- (Optional) configure lua language server
+          local lua_opts = lsp_zero.nvim_lua_ls()
+          require('lspconfig').lua_ls.setup(lua_opts)
+        end,
+      }
+    })
+  end
+}
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,35 +1,44 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-   ensure_installed = {
-      "bash",
-      "css",
-      "go",
-      "graphql",
-      "html",
-      "javascript",
-      "jsdoc",
-      "json",
-      "json5",
-      "lua",
-      "markdown",
-      "regex",
-      "rust",
-      "scss",
-      "sql",
-      "svelte",
-      "terraform",
-      "toml",
-      "tsx",
-      "typescript",
-      "vim",
-      "vue",
-      "yaml",
-      "zig",
-    },
-    highlight = {
-      enable = true
-    },
-    indent = {
-      enable = true,
-    },
+  build = ":TSUpdate",
+    opts = {
+      highlight = {
+        enable = true,
+
+        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        -- Instead of true it can also be a list of languages
+        additional_vim_regex_highlighting = false,
+      },
+      indent = {
+        enable = true,
+      },
+      ensure_installed = {
+        "bash",
+        "css",
+        "go",
+        "graphql",
+        "html",
+        "javascript",
+        "jsdoc",
+        "json",
+        "json5",
+        "lua",
+        "markdown",
+        "regex",
+        "rust",
+        "scss",
+        "sql",
+        "svelte",
+        "terraform",
+        "toml",
+        "tsx",
+        "typescript",
+        "vim",
+        "vue",
+        "yaml",
+        "zig",
+      },
+    }
 }
