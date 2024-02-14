c35de7f99ef9343524e8d44d2a067c8edb5ad4fe
Vicente <pouyio@gmail.com>
2024-02-14 16:10:36 +0100

    added illuminate plugin to highlight other references

--- lua/plugins/illuminate.lua
+++ lua/plugins/illuminate.lua
@@ -4,9 +4,9 @@ return {
   event = {"BufReadPost", "BufNewFile"},
   config = function (_, opts)
     require("illuminate").configure(opts)
-    vim.api.nvim_set_hl(0, "IlluminatedWordText", { bg = "#4f535b" })
-    vim.api.nvim_set_hl(0, "IlluminatedWordRead", { bg = "#4f535b" })
-    vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { bg = "#4f535b" })
+    vim.api.nvim_set_hl(0, "IlluminatedWordText", { link = "Visual" })
+    vim.api.nvim_set_hl(0, "IlluminatedWordRead", {  link = "Visual" })
+    vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { link = "Visual" })
   end
 }
 
