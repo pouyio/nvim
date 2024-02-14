2eecc6d8e2fec0e452c3d92cd33c56b66cd29ae6
Vicente <pouyio@gmail.com>
2024-02-15 00:38:55 +0100

    added scrollbar

---
+++ lua/plugins/nvim-hlslens.lua
@@ -0,0 +1,20 @@
+return {
+  "kevinhwang91/nvim-hlslens",
+  config = function ()
+    local kopts = {noremap = true, silent = true}
+    vim.api.nvim_set_keymap('n', 'n',
+    [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]],
+    kopts)
+vim.api.nvim_set_keymap('n', 'N',
+    [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]],
+    kopts)
+    vim.api.nvim_set_keymap('n', '*', [[*<Cmd>lua require('hlslens').start()<CR>]], kopts)
+      require("scrollbar.handlers.search").setup({
+        calm_down = true, -- hide highlight when moving cursor out of one
+        override_lens = function ()
+          -- dont show any text on the lense  
+        end
+      })
+
+  end
+}
---
+++ lua/plugins/vim-scrollbar.lua
@@ -0,0 +1,14 @@
+return {
+  "petertriho/nvim-scrollbar",
+  opts = {
+    -- overriding because onedark theme messes them up
+    marks = {
+      Search = { color = "#dd9046" },
+      Error = { color = "#c53b53" },
+      Warn = { color = "#ffc777" },
+      Info = { color = "#0db9d7" },
+      Hint = { color = "#4fd6be" },
+      Misc = { color = "#c75ae8" },
+  }
+  }
+}
