0facc4af44a63f32125cb1a431260a837b2476c0
Vicente <pouyio@gmail.com>
2024-02-14 16:19:22 +0100

    added fuzzy find to telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,18 +1,27 @@
 return {
-    'nvim-telescope/telescope.nvim', branch = '0.1.x',
-      dependencies = { 'nvim-lua/plenary.nvim' },
-  config = function()
-    local builtin = require("telescope.builtin")
-    local telescope = require("telescope")
+  'nvim-telescope/telescope.nvim', branch = '0.1.x',
+    dependencies = {
+      {'nvim-lua/plenary.nvim' },
+      {
+        'nvim-telescope/telescope-fzf-native.nvim',
+         build = "make",
+         config = function()
+           require("telescope").load_extension("fzf")
+         end,
+    }
+},
+config = function()
+  local builtin = require("telescope.builtin")
+  local telescope = require("telescope")
 
-    telescope.setup {
-      defaults = {
-        layout_config = {width = 0.95}
-      },
-    }
-    vim.keymap.set("n", "<leader>p", builtin.find_files, { desc = "Find Files" })
-    vim.keymap.set('n', '<leader>f', builtin.live_grep, { desc = "Grep String" })
-    -- vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Grep word" })
-    -- vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "Find old files" })
-  end
-    }
+  telescope.setup {
+    defaults = {
+      layout_config = {width = 0.95}
+    },
+  }
+  vim.keymap.set("n", "<leader>p", builtin.find_files, { desc = "Find Files" })
+  vim.keymap.set('n', '<leader>f', builtin.live_grep, { desc = "Grep String" })
+  -- vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Grep word" })
+  -- vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "Find old files" })
+end
+  }
