06507ebbb9bd18f820775edd87e80147d40efb17
Vicente <pouyio@gmail.com>
2024-02-15 01:28:20 +0100

    improve lualine ui

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,6 +1,15 @@
 return {
   "nvim-lualine/lualine.nvim",
   dependencies= {"nvim-tree/nvim-web-devicons"},
-  opts = {
-  },
+  config = function()
+    local options = {
+      sections = {
+        lualine_y = {},
+      },
+      extensions = {
+        'nvim-tree',
+      },
+    }
+    require("lualine").setup(options)
+  end,
 }
