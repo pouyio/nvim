cf6b4b4a6a0d2ff65cfb1abfdb2bb609cd0856b6
Vicente <pouyio@gmail.com>
2024-02-16 14:29:39 +0100

    added macos shortcuts to multi cursor

--- lua/plugins/vim-visual-multi.lua
+++ lua/plugins/vim-visual-multi.lua
@@ -2,8 +2,18 @@ return {
   "mg979/vim-visual-multi",
   init = function()
     vim.g.VM_maps = {
-      ["Find Under"] = "<C-d>"
+      ["Find Under"] = "<D-d>",
+      ["Find Subword Under"] = "<D-d>",
+      ["Add Cursor Down"] = "<D-A-Down>",
+      ["Add Cursor Up"] = "<D-A-Up>",
+      ["Undo"] = "u"
     }
     vim.g.VM_theme = "ocean"
+    vim.api.nvim_set_var('VM_custom_noremaps', {
+      ['<D-Right>'] = '$',
+      ['<D-Left>'] = '_',
+      ['<A-Right>'] = 'w',
+      ['<A-Left>'] = 'b',
+    })
   end
 }
