134d50006b96d73bc9a8a836bde3221b808a6001
Vicente <pouyio@gmail.com>
2024-02-20 21:18:00 +0100

    fixed win/mac keymaps

--- lua/plugins/vim-visual-multi.lua
+++ lua/plugins/vim-visual-multi.lua
@@ -3,10 +3,10 @@ return {
   init = function()
     local f = require("plugins.common.utils")
     vim.g.VM_maps = {
-      ["Find Under"] = f.isMac() and "<C-d>" or "<D-d>",
-      ["Find Subword Under"] = f.isMac() and "<C-d>" or "<D-d>",
-      ["Add Cursor Down"] = f.isMac() and "<C-A-Down>" or "<D-A-Down>",
-      ["Add Cursor Up"] = f.isMac() and "<C-A-Up>" or "<D-A-Up>",
+      ["Find Under"] = f.isMac() and "<D-d>" or "<C-d>",
+      ["Find Subword Under"] = f.isMac() and "<D-d>" or "<C-d>",
+      ["Add Cursor Down"] = f.isMac() and "<D-A-Down>" or "<C-A-Down>",
+      ["Add Cursor Up"] = f.isMac() and "<D-A-Up>" or "<C-A-Up>",
       ["Undo"] = "u"
     }
     vim.g.VM_theme = "ocean"
