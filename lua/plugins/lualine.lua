0ef83c6b6d1157b3f68e7eeff04389ab96f0fa9a
Vicente <pouyio@gmail.com>
2024-02-20 15:52:38 +0100

    removed bufferline, used lualine instead

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,14 +1,15 @@
 return {
   "nvim-lualine/lualine.nvim",
-  dependencies= {"nvim-tree/nvim-web-devicons"},
+  dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     local options = {
       sections = {
+        lualine_a = { "mode", "buffers" },
         lualine_y = {},
       },
       extensions = {
         'nvim-tree',
-      },
+      }
     }
     require("lualine").setup(options)
   end,
