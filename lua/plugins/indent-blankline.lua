ad3ad7328b258db721a1387df31e3d7d3de0947d
Vicente <pouyio@gmail.com>
2024-02-20 23:42:59 +0100

    formatted file

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,7 +1,7 @@
-return { "lukas-reineke/indent-blankline.nvim",
+return {
+  "lukas-reineke/indent-blankline.nvim",
   main = "ibl",
   config = function()
-
-   require("ibl").setup()
+    require("ibl").setup()
   end
 }
