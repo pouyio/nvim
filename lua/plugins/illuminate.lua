0fc5494b0766161bdb2b22a9521659a2e59dd04f
Vicente <pouyio@gmail.com>
2024-02-20 20:00:27 +0100

    formatted file

--- lua/plugins/illuminate.lua
+++ lua/plugins/illuminate.lua
@@ -1,17 +1,15 @@
 return {
   "RRethy/vim-illuminate",
   enabled = true,
-  event = {"BufReadPost", "BufNewFile"},
-  config = function (_, opts)
+  event = { "BufReadPost", "BufNewFile" },
+  config = function(_, opts)
     require("illuminate").configure(opts)
     vim.api.nvim_set_hl(0, "IlluminatedWordText", { link = "Visual" })
-    vim.api.nvim_set_hl(0, "IlluminatedWordRead", {  link = "Visual" })
+    vim.api.nvim_set_hl(0, "IlluminatedWordRead", { link = "Visual" })
     vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { link = "Visual" })
   end
 }
 
--- bg = "#8AADF4", fg = "#24273A" 
--- bg = "#A6DA95", fg = "#24273A" 
--- bg = "#ED8796", fg = "#24273A" 
-
-
+-- bg = "#8AADF4", fg = "#24273A"
+-- bg = "#A6DA95", fg = "#24273A"
+-- bg = "#ED8796", fg = "#24273A"
