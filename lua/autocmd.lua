bed050f6ccaf52a6fcc15ceace7641a01f225914
Vicente <pouyio@gmail.com>
2024-03-04 18:40:19 +0100

    added highlight on yank

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -26,3 +26,11 @@ vim.api.nvim_create_autocmd("CursorMovedI", {
 		require("custom-blamer").clearBlameVirtText()
 	end,
 })
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking text",
+	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
