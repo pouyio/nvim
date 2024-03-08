d29ca72cdbf39f8fb7de67dc014550950f172305
Vicente <pouyio@gmail.com>
2024-03-08 08:08:25 +0100

    moved line blame to gitsigns

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,7 +1,14 @@
 return {
-  "lewis6991/gitsigns.nvim",
-  config = function ()
-    require("gitsigns").setup()
-    require("scrollbar.handlers.gitsigns").setup()
-  end
+	"lewis6991/gitsigns.nvim",
+	config = function()
+		local opts = {
+			current_line_blame = true,
+			current_line_blame_opts = {
+				delay = 300,
+			},
+			current_line_blame_formatter = "<author>, <author_time:%R> - <summary>",
+		}
+		require("gitsigns").setup(opts)
+		require("scrollbar.handlers.gitsigns").setup()
+	end,
 }
