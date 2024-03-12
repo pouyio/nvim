ff006e8b36713109f320a3179a2fa7b365e183b6
Vicente <pouyio@gmail.com>
2024-03-12 11:29:47 +0100

    added lazygit keymap

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -2,11 +2,25 @@ local f = require("plugins.common.utils")
 return {
 	"akinsho/toggleterm.nvim",
 	version = "v2.*",
-	opts = {
-		direction = "float",
-		open_mapping = f.isMac() and [[<D-j>]] or [[<C-j>]],
-		float_opts = {
-			border = "curved",
-		},
-	},
+	config = function()
+		local opts = {
+			direction = "float",
+			open_mapping = f.isMac() and [[<D-j>]] or [[<C-j>]],
+			float_opts = {
+				border = "curved",
+			},
+		}
+		require("toggleterm").setup(opts)
+		local Terminal = require("toggleterm.terminal").Terminal
+		local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+		function _lazygit_toggle()
+			lazygit:toggle()
+		end
+		vim.api.nvim_set_keymap(
+			"n",
+			f.isMac() and "<D-l>" or "<C-l>",
+			"<cmd>lua _lazygit_toggle()<CR>",
+			{ noremap = true, silent = true }
+		)
+	end,
 }
