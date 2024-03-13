ea02e2c0412918f5ff2a5c2d396c074768342f3d
Vicente <pouyio@gmail.com>
2024-03-13 21:48:02 +0100

    added search signs to scrollbar

--- lua/plugins/vim-scrollbar.lua
+++ lua/plugins/vim-scrollbar.lua
@@ -1,14 +1,17 @@
 return {
-  "petertriho/nvim-scrollbar",
-  opts = {
-    -- overriding because onedark theme messes them up
-    marks = {
-      Search = { color = "#dd9046" },
-      Error = { color = "#c53b53" },
-      Warn = { color = "#ffc777" },
-      Info = { color = "#0db9d7" },
-      Hint = { color = "#4fd6be" },
-      Misc = { color = "#c75ae8" },
-  }
-  }
+	"petertriho/nvim-scrollbar",
+	opts = {
+		-- overriding because onedark theme messes them up
+		marks = {
+			Search = { color = "#dd9046" },
+			Error = { color = "#c53b53" },
+			Warn = { color = "#ffc777" },
+			Info = { color = "#0db9d7" },
+			Hint = { color = "#4fd6be" },
+			Misc = { color = "#c75ae8" },
+		},
+		handlers = {
+			search = true,
+		},
+	},
 }
