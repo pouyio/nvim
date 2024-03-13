ca6913d1e765ef92f538c502b51fc33800743e42
Vicente <pouyio@gmail.com>
2024-03-13 21:08:05 +0100

    added folder name to lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,18 +1,31 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    local options = {
-      sections = {
-        lualine_c = {
-          { "filename", path = 1 }
-        },
-        lualine_y = {},
-      },
-      extensions = {
-        'nvim-tree',
-      }
-    }
-    require("lualine").setup(options)
-  end,
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local options = {
+			sections = {
+				lualine_b = {
+					function()
+						-- Get the current working directory
+						local currentDir = vim.fn.getcwd()
+
+						-- Extract the last folder from the path
+						local lastFolder = currentDir:match("[^/]+$")
+						return lastFolder
+					end,
+					"branch",
+					"diff",
+					"diagnostics",
+				},
+				lualine_c = {
+					{ "filename", path = 1 },
+				},
+				lualine_y = {},
+			},
+			extensions = {
+				"nvim-tree",
+			},
+		}
+		require("lualine").setup(options)
+	end,
 }
