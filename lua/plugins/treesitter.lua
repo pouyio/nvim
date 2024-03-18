6595dd0843a38a577e4116b59c5651e509ef631c
Vicente <pouyio@gmail.com>
2024-03-18 13:40:38 +0100

    formatted

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,55 +1,54 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    local f = require("plugins.common.utils")
-    local treesitter = require("nvim-treesitter.configs")
-    treesitter.setup({
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local f = require("plugins.common.utils")
+		local treesitter = require("nvim-treesitter.configs")
+		treesitter.setup({
 
-      highlight = {
-        enable = true,
+			highlight = {
+				enable = true,
 
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        -- Instead of true it can also be a list of languages
-        additional_vim_regex_highlighting = false,
-      },
-      auto_install = true,
-      indent = {
-        enable = true,
-      },
-      ensure_installed = {
-        "bash",
-        "css",
-        "go",
-        "graphql",
-        "html",
-        "javascript",
-        "jsdoc",
-        "json",
-        "json5",
-        "lua",
-        "markdown",
-        "regex",
-        "scss",
-        "sql",
-        "terraform",
-        "toml",
-        "tsx",
-        "typescript",
-        "vue",
-        "yaml",
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = f.isMac() and "<D-A-Right>" or "<C-A-Right>",
-          node_incremental = f.isMac() and "<D-A-Right>" or "<C-A-Right>",
-          node_decremental = f.isMac() and "<D-A-Left>" or "<C-A-Left>",
-        },
-      },
-
-    })
-  end
+				-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+				-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+				-- Using this option may slow down your editor, and you may see some duplicate highlights.
+				-- Instead of true it can also be a list of languages
+				additional_vim_regex_highlighting = false,
+			},
+			auto_install = true,
+			indent = {
+				enable = true,
+			},
+			ensure_installed = {
+				"bash",
+				"css",
+				"go",
+				"graphql",
+				"html",
+				"javascript",
+				"jsdoc",
+				"json",
+				"json5",
+				"lua",
+				"markdown",
+				"regex",
+				"scss",
+				"sql",
+				"terraform",
+				"toml",
+				"tsx",
+				"typescript",
+				"vue",
+				"yaml",
+			},
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = f.isMac() and "<D-A-Right>" or "<C-A-Right>",
+					node_incremental = f.isMac() and "<D-A-Right>" or "<C-A-Right>",
+					node_decremental = f.isMac() and "<D-A-Left>" or "<C-A-Left>",
+				},
+			},
+		})
+	end,
 }
