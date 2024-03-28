6d341ee128f451f9d135c9fd79c14a7e60d1e2c9
Vicente <pouyio@gmail.com>
2024-03-28 14:21:48 +0100

    change theme

--- lua/plugins/onedark.lua
+++ lua/plugins/onedark.lua
@@ -1,15 +1,31 @@
 return {
-	"navarasu/onedark.nvim",
-	config = function()
-		require("onedark").setup({
-			style = "darker",
-			code_style = {
-				functions = "bold",
-			},
-			colors = {
-				bg3 = "#4c5263",
-			},
-		})
-		vim.cmd.colorscheme("onedark")
-	end,
+	{
+		"navarasu/onedark.nvim",
+		config = function()
+			require("onedark").setup({
+				style = "darker",
+				code_style = {
+					functions = "bold",
+				},
+				colors = {
+					bg3 = "#4c5263",
+				},
+			})
+			-- vim.cmd.colorscheme("onedark")
+		end,
+	},
+	{
+		"olimorris/onedarkpro.nvim",
+		config = function()
+			require("onedarkpro").setup({
+				colors = {
+					bg = "#1f232d",
+				},
+				styles = {
+					functions = "bold",
+				},
+			})
+			vim.cmd.colorscheme("onedark_vivid")
+		end,
+	},
 }
