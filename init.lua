7ce6a8bde1b6c6717fa9ca8b543f9fff704c0740
Vicente <pouyio@gmail.com>
2024-04-02 23:38:16 +0200

    moved options declaration to top as it not depends on anything

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
+require("options")
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
@@ -11,7 +13,6 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("options")
 require("keymaps")
 require("autocmd")
 require("commands")
