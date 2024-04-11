c12cedfcf9c78489539ab7c8590a57db16127488
Vicente <pouyio@gmail.com>
2024-04-11 14:13:12 +0200

    removed telescope integration from harpoon, cant reorder files

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -6,33 +6,11 @@ return {
 		local harpoon = require("harpoon")
 		harpoon.setup()
 
-		-- basic telescope configuration
-		local conf = require("telescope.config").values
-		local function toggle_telescope(harpoon_files)
-			local file_paths = {}
-			for _, item in ipairs(harpoon_files.items) do
-				table.insert(file_paths, item.value)
-			end
-
-			require("telescope.pickers")
-				.new({}, {
-					prompt_title = "Harpoon",
-					initial_mode = "normal",
-					finder = require("telescope.finders").new_table({
-						results = file_paths,
-					}),
-					previewer = conf.file_previewer({}),
-					sorter = conf.generic_sorter({}),
-				})
-				:find()
-		end
-
 		vim.keymap.set("n", "<leader>a", function()
 			harpoon:list():add()
 		end)
 		vim.keymap.set("n", "<leader>tt", function()
-			-- harpoon.ui:toggle_quick_menu(harpoon:list())
-			toggle_telescope(harpoon:list())
+			harpoon.ui:toggle_quick_menu(harpoon:list())
 		end)
 
 		vim.keymap.set("n", "<leader>h", function()
