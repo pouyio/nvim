954fd94a76ca7e3d25790c7afcfb2bcb39b2807e
Vicente <pouyio@gmail.com>
2024-04-12 11:04:30 +0200

    improved comment plugin

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -8,12 +8,13 @@ return {
 	},
 	config = function()
 		local comment = require("Comment")
-		local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
-
+		require("ts_context_commentstring").setup({
+			enable_autocmd = false,
+		})
 		-- enable comment
 		comment.setup({
 			-- for commenting tsx, jsx, svelte, html files
-			pre_hook = ts_context_commentstring.create_pre_hook(),
+			pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
 			toggler = {
 				line = f.isMac() and "<D-u>" or "<C-u>",
 			},
