a23db8ea41cd83b405804491f27a2c1287653cbf
Vicente <pouyio@gmail.com>
2024-04-23 14:22:35 +0200

    typo

--- lua/plugins/nvim-surround.lua
+++ lua/plugins/nvim-surround.lua
@@ -3,10 +3,10 @@ return {
 	version = "*", -- Use for stability; omit to use `main` branch for the latest features
 	event = "VeryLazy",
 	config = function()
-		-- unding requires several actions, maybe it can be improved
-		vim.keymap.set("v", "(", "<plug>(nvim-surround-visual)(lvi(", { desc = "toggle last buffer" })
-		vim.keymap.set("v", "[", "<Plug>(nvim-surround-visual)[lvi[", { desc = "Toggle last buffer" })
-		vim.keymap.set("v", "{", "<Plug>(nvim-surround-visual){lvi{", { desc = "Toggle last buffer" })
+		-- undoing requires several actions, maybe it can be improved
+		vim.keymap.set("v", "(", "<plug>(nvim-surround-visual)(lvi(", { desc = "Surround with brackets ()" })
+		vim.keymap.set("v", "[", "<Plug>(nvim-surround-visual)[lvi[", { desc = "Surround with square brackets []" })
+		vim.keymap.set("v", "{", "<Plug>(nvim-surround-visual){lvi{", { desc = "Surround with curly brackets {}" })
 		require("nvim-surround").setup({
 			surrounds = {
 				["("] = {
