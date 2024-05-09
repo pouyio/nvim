c85cc2fa20c8556060ea5d3b5c7d2ba7de905cd6
Vicente <pouyio@gmail.com>
2024-05-09 20:48:49 +0200

    improved windows indent line

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,7 +1,9 @@
 return {
-  "lukas-reineke/indent-blankline.nvim",
-  main = "ibl",
-  config = function()
-    require("ibl").setup()
-  end
+	"lukas-reineke/indent-blankline.nvim",
+	main = "ibl",
+	config = function()
+		local f = require("plugins.common.utils")
+		local opts = f.isMac() and {} or { indent = { char = "│" } }
+		require("ibl").setup(opts)
+	end,
 }
