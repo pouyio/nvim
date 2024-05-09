615b18631645c3a09393296a9fb3451b9600a102
Vicente <pouyio@gmail.com>
2024-05-09 13:16:43 +0200

    updated dep

--- lua/plugins/nvim-surround.lua
+++ lua/plugins/nvim-surround.lua
@@ -3,11 +3,11 @@ return {
 	version = "*", -- Use for stability; omit to use `main` branch for the latest features
 	event = "VeryLazy",
 	config = function()
-		-- undoing requires several actions, maybe it can be improved
 		vim.keymap.set("v", "(", "<plug>(nvim-surround-visual)(lvi(", { desc = "Surround with brackets ()" })
 		vim.keymap.set("v", "[", "<Plug>(nvim-surround-visual)[lvi[", { desc = "Surround with square brackets []" })
 		vim.keymap.set("v", "{", "<Plug>(nvim-surround-visual){lvi{", { desc = "Surround with curly brackets {}" })
 		vim.keymap.set("v", '"', '<Plug>(nvim-surround-visual)"lvi"', { desc = 'Surround with curly brackets ""' })
+		vim.keymap.set("v", "`", "<Plug>(nvim-surround-visual)`lvi`", { desc = "Surround with curly brackets ``" })
 		require("nvim-surround").setup({
 			surrounds = {
 				["("] = {
