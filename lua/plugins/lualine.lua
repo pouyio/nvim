0df5044f16009b5e96a6aa2b18dfbca6f918aa19
Vicente <pouyio@gmail.com>
2024-05-14 13:12:46 +0200

    shortened mode in lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -12,8 +12,36 @@ return {
 			end,
 		})
 
+		local mode_map = {
+			["NORMAL"] = "N",
+			["O-PENDING"] = "N?",
+			["INSERT"] = "I",
+			["VISUAL"] = "V",
+			["V-BLOCK"] = "VB",
+			["V-LINE"] = "VL",
+			["V-REPLACE"] = "VR",
+			["REPLACE"] = "R",
+			["COMMAND"] = "!",
+			["SHELL"] = "SH",
+			["TERMINAL"] = "T",
+			["EX"] = "X",
+			["S-BLOCK"] = "SB",
+			["S-LINE"] = "SL",
+			["SELECT"] = "S",
+			["CONFIRM"] = "Y?",
+			["MORE"] = "M",
+		}
+
 		local options = {
 			sections = {
+				lualine_a = {
+					{
+						"mode",
+						fmt = function(s)
+							return mode_map[s] or s
+						end,
+					},
+				},
 				lualine_b = {
 					function()
 						-- Get the current working directory
