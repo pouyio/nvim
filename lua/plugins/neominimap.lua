aca91ed2b0978876a97d2f17bd5eb60ece88bf64
Vicente <pouyio@gmail.com>
2024-08-19 21:42:02 +0200

    updated deps and installed minimap

---
+++ lua/plugins/neominimap.lua
@@ -0,0 +1,23 @@
+return {
+	"Isrothy/neominimap.nvim",
+	enabled = true,
+	lazy = false, -- NOTE: NO NEED to Lazy load
+	-- Optional
+	keys = {},
+	init = function()
+		vim.opt.wrap = false -- Recommended
+		vim.opt.sidescrolloff = 36 -- It's recommended to set a large value
+		vim.g.neominimap = {
+			minimap_width = 13,
+			auto_enable = true,
+			click = {
+				enabled = true,
+				auto_switch_focus = false,
+			},
+			search = {
+				enabled = true,
+				mode = "sign",
+			},
+		}
+	end,
+}
