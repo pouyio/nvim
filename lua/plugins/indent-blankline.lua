a9b1bbeff59fc79b158671243a2b298e127f9aee
Vicente <pouyio@gmail.com>
2024-11-14 12:33:18 +0100

    updated indentation config

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,9 +1,7 @@
 return {
 	"lukas-reineke/indent-blankline.nvim",
 	main = "ibl",
-	config = function()
-		local f = require("plugins.common.utils")
-		local opts = f.isMac() and {} or { indent = { char = "│" } }
-		require("ibl").setup(opts)
-	end,
+	opts = {
+		indent = { char = "│" },
+	},
 }
