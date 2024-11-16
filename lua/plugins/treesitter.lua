dd12c6e0ab851149bb68589a7e4bec10541f08d7
Vicente <pouyio@gmail.com>
2024-11-16 09:51:46 +0100

    fix windows shortcut

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -50,7 +50,7 @@ return {
 				keymaps = {
 					init_selection = "<CR>",
 					node_incremental = "<CR>",
-					node_decremental = f.isMac() and "<S-CR>" or "<up>", -- mapped in windows terminal because it does not send shift+space
+					node_decremental = f.isMac() and "<S-CR>" or "⊗", -- strange unicode mapped in windows because it cant understand shift+enter
 				},
 			},
 		})
