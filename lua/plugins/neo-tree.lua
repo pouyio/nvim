9049773eb006ee2d6aefc2245f7590e92e0a59c6
Vicente <pouyio@gmail.com>
2024-11-29 06:54:19 +0100

    remove dead code

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -88,23 +88,6 @@ return {
 					enabled = true,
 				},
 			},
-			default_component_configs = {
-				git_status = {
-					symbols = {
-						-- Change type
-						added = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
-						modified = "", -- or "", but this is redundant info if you use git_status_colors on the name
-						deleted = "✖", -- this can only be used in the git_status source
-						renamed = "󰁕", -- this can only be used in the git_status source
-						-- Status type
-						untracked = "",
-						ignored = "",
-						unstaged = "󰄱",
-						staged = "",
-						conflict = "",
-					},
-				},
-			},
 			event_handlers = {
 				{
 					event = events.FILE_OPENED,
