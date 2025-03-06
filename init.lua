8e146dc9142e2f9f7952d3a62f0a92d6d249113b
Vicente <pouyio@gmail.com>
2025-03-06 12:48:55 +0100

    test vscode plugin

--- init.lua
+++ init.lua
@@ -1,20 +1,24 @@
-require("options")
+if vim.g.vscode then
+	require("vscode_config")
+else
+	require("options")
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+	local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+	if not vim.loop.fs_stat(lazypath) then
+		vim.fn.system({
+			"git",
+			"clone",
+			"--filter=blob:none",
+			"https://github.com/folke/lazy.nvim.git",
+			"--branch=stable", -- latest stable release
+			lazypath,
+		})
+	end
+	vim.opt.rtp:prepend(lazypath)
+
+	require("keymaps")
+	require("autocmd")
+	require("commands")
+
+	require("lazy").setup("plugins", { change_detection = { notify = false } })
 end
-vim.opt.rtp:prepend(lazypath)
-
-require("keymaps")
-require("autocmd")
-require("commands")
-
-require("lazy").setup("plugins", { change_detection = { notify = false } })
