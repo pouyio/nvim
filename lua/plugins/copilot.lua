ce5b89e5e0bc96599fe7e11e2d8dd9a895fbbc91
Vicente <pouyio@gmail.com>
2025-05-15 12:39:08 +0200

    testing codecompanion & copilot.lua

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,5 +1,72 @@
 local f = require("plugins.common.utils")
 return {
-	"github/copilot.vim",
-	enabled = f.isMac(),
+	{
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		event = "InsertEnter",
+		enabled = f.isMac(),
+		opts = {
+			suggestion = {
+				auto_trigger = true,
+				keymap = {
+					accept = "<Tab>",
+				},
+			},
+		},
+		config = function(_, opts)
+			require("copilot").setup(opts)
+		end,
+	},
+	{
+		"olimorris/codecompanion.nvim",
+		event = "VeryLazy",
+		enabled = f.isMac(),
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		opts = {
+			adapters = {
+				copilot = function()
+					return require("codecompanion.adapters").extend("copilot", {
+						schema = {
+							model = {
+								default = "claude-3.7-sonnet",
+							},
+						},
+					})
+				end,
+			},
+			strategies = {
+				chat = {
+					keymaps = {
+						stop = {
+							modes = {
+								n = "<C-c>", -- Changed from "q" to "<C-q>"
+							},
+						},
+					},
+				},
+			},
+		},
+		config = function(_, opts)
+			require("codecompanion").setup(opts)
+			vim.keymap.set(
+				{ "n", "v" },
+				f.isMac() and "<D-k>" or "<C-k>",
+				"<cmd>CodeCompanionChat Toggle<CR>",
+				{ desc = "Code Companion Chat" }
+			)
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = "codecompanion",
+				callback = function(event)
+					vim.keymap.set({ "n", "v" }, "q", "<cmd>CodeCompanionChat Toggle<CR>", {
+						buffer = event.buf,
+						desc = "Toggle CodeCompanion Chat",
+						nowait = true,
+					})
+				end,
+			})
+		end,
+	},
 }
