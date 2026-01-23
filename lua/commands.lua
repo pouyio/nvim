a94a6096f55f7e8b6682c4b61d333c986aa669d9
Vicente <pouyio@gmail.com>
2026-01-23 12:21:16 +0100

    improve quickfixtoggle command

--- lua/commands.lua
+++ lua/commands.lua
@@ -1,7 +1,19 @@
 local create_cmd = vim.api.nvim_create_user_command
 
 create_cmd("QuickfixListToggle", function()
-	vim.cmd("copen")
+	local qf_exists = false
+	for _, win in pairs(vim.fn.getwininfo()) do
+		if win.quickfix == 1 then
+			qf_exists = true
+			break
+		end
+	end
+
+	if qf_exists then
+		vim.cmd("cclose")
+	else
+		vim.cmd("copen")
+	end
 end, { desc = "Toggle quickfix list" })
 
 create_cmd("ClearAllMarks", function()
