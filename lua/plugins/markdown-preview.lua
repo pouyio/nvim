583b5843c694b16d6e1dbd24ee9c19d3f4adfec3
Vicente <pouyio@gmail.com>
2026-02-14 17:59:02 +0100

    fixed markdown preview

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,8 +1,12 @@
 return {
-	"iamcco/markdown-preview.nvim",
-	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-	ft = { "markdown" },
-	build = function()
-		vim.fn["mkdp#util#install"]()
+	"selimacerbas/markdown-preview.nvim",
+	dependencies = { "selimacerbas/live-server.nvim" },
+	config = function()
+		require("markdown_preview").setup({
+			-- all optional; sane defaults shown
+			port = 8421,
+			open_browser = true,
+			debounce_ms = 300,
+		})
 	end,
 }
