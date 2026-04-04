6126a7cf866c1cf1a3e39e1c464aaf1d830bc2df
Vicente <pouyio@gmail.com>
2026-04-04 12:40:23 +0200

    bring back some borders

--- lua/options.lua
+++ lua/options.lua
@@ -26,6 +26,7 @@ vim.filetype.add({
 	},
 })
 vim.o.mousescroll = f.isMac() and "ver:1,hor:1" or "ver:3,hor:3"
+vim.o.winborder = "rounded"
 
 -- Reserve a space in the gutter
 -- This will avoid an annoying layout shift in the screen
