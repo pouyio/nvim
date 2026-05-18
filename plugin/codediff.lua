6ee81f5d8c692947ace92764ca4460befa7e41cd
Vicente <pouyio@gmail.com>
2026-05-18 12:00:06 +0200

    fix navigate hunks in codediff

--- plugin/codediff.lua
+++ plugin/codediff.lua
@@ -2,10 +2,32 @@ vim.pack.add({
 	"https://github.com/esmuellert/codediff.nvim",
 	"https://github.com/MunifTanjim/nui.nvim",
 })
-vim.keymap.set("n", "<leader>dd", "<cmd>CodeDiff<cr>", { desc = "Code diff" })
+vim.keymap.set("n", "<leader>dD", "<cmd>CodeDiff<cr>", { desc = "Code diff" })
+vim.keymap.set("n", "<leader>dd", function()
+	local base
+	for _, branch in ipairs({ "main", "master" }) do
+		vim.fn.system("git rev-parse --verify " .. branch .. " 2>/dev/null")
+		if vim.v.shell_error == 0 then
+			base = branch
+			break
+		end
+	end
+	if not base then
+		vim.notify("No main or master branch found", vim.log.levels.WARN)
+		return
+	end
+	vim.cmd("CodeDiff " .. base .. "...")
+end, { desc = "Code diff vs main/master" })
 require("codediff").setup({
 	explorer = { view_mode = "tree" },
 	keymaps = {
-		view = { next_file = "<S-Down>", prev_file = "<S-Up>" },
+		view = {
+			next_file = "<S-Down>",
+			prev_file = "<S-Up>",
+			next_hunk = "<A-l>",
+			prev_hunk = "<A-h>",
+			open_in_prev_tab = "e",
+			close_on_open_in_prev_tab = true,
+		},
 	},
 })
--- plugin/gitsigns.lua
+++ plugin/gitsigns.lua
@@ -13,12 +13,17 @@ local opts = {
 		topdelete = { text = "" },
 	},
 	on_attach = function(bufnr)
-		vim.keymap.set("n", "<A-h>", function()
-			gitsigns.nav_hunk("prev")
-		end, { buffer = bufnr })
-		vim.keymap.set("n", "<A-l>", function()
-			gitsigns.nav_hunk("next")
-		end, { buffer = bufnr })
+		-- Avoid setting the keymap before codediff to avoid conflict
+		local ok, lifecycle = pcall(require, "codediff.ui.lifecycle")
+		local in_codediff = ok and lifecycle.find_tabpage_by_buffer(bufnr)
+		if not in_codediff then
+			vim.keymap.set("n", "<A-h>", function()
+				gitsigns.nav_hunk("prev")
+			end, { buffer = bufnr })
+			vim.keymap.set("n", "<A-l>", function()
+				gitsigns.nav_hunk("next")
+			end, { buffer = bufnr })
+		end
 		vim.keymap.set("n", "<leader>gi", gitsigns.preview_hunk)
 		vim.keymap.set("n", "<leader>gu", gitsigns.reset_hunk)
 		vim.keymap.set("n", "<leader>gs", gitsigns.stage_hunk)
